fa52d1aa3113326020baee31a516850e24c8affc
Marc Howell <sapient@microbump.com>
2022-09-06 22:56:36 +0200

    Fix devise login working with Turbo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_s
+    when 'notice' then "alert-primary"
+    when 'success' then "alert-success"
+    when 'error' then "alert-danger"
+    when 'alert' then "alert-warning"
+    else 'alert-dark'
+    end
+  end
 end
