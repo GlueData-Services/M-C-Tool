31c8a0636897dcbedf2b1e039fc92e20d763d8c8
Marc Howell <sapient@microbump.com>
2022-10-13 11:45:46 +0200

    Error filtering on consolidation list

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -28,12 +28,43 @@ module ValidationHelper
 
   def summarize_errors(match)
     errors = []
+    css_class = "badge bg-light text-dark border-0 border-dark"
     match.matched_articles.each do |art|
-      errors << content_tag(:span, "#{art.prefixed_matnr} DUPE", class: 'badge bg-danger') if art.duplicate?
-      errors << content_tag(:span, "#{art.prefixed_matnr} BADCAT", class: 'badge bg-danger') if art.bad_category?
-      errors << content_tag(:span, "#{art.prefixed_matnr} BADMAT", class: 'badge bg-danger') if art.bad_material?
-      errors << content_tag(:span, "#{art.prefixed_matnr} UOM", class: 'badge bg-danger') if art.uom_mismatch?
+      errors << content_tag(:span, "#{art.prefixed_matnr} DUPE", class: css_class) if art.duplicate?
+      errors << content_tag(:span, "#{art.prefixed_matnr} BADCAT", class: css_class) if art.bad_category?
+      errors << content_tag(:span, "#{art.prefixed_matnr} BADMAT", class: css_class) if art.bad_material?
+      errors << content_tag(:span, "#{art.prefixed_matnr} UOM", class: css_class) if art.uom_mismatch?
+      errors << content_tag(:span, "#{art.prefixed_matnr} UOMREV", class: css_class) if art.uom_review?
+      errors << content_tag(:span, "#{art.prefixed_matnr} TAXDIFF", class: css_class) if art.tax_diff?
     end
     errors.join(' ').html_safe
+  end
+
+  def summarize_notes(match)
+    notes = []
+    match.matched_articles.each do |art|
+      notes << content_tag(:span, art.comment)
+    end
+    notes.join(' ').html_safe
+  end
+
+  def error_filter_select
+    opts = [
+      ['Bad Category', 'bad_category'],
+      ['UoM Mismatch', 'uom_mismatch'],
+      ['Bad Material', 'bad_material'],
+      ['Duplicate', 'duplicate'],
+      ['Tax Difference', 'tax_diff'],
+      ['AUoM Review', 'uom_review'],
+    ]
+    select_tag(:error_filter,
+               options_for_select(opts, params[:error_filter]),
+               include_blank: true,
+               prompt: 'Error Filter',
+               class: 'form-select',
+               data: {
+                 controller: 'filter',
+                 action: 'filter#changed'
+               })
   end
 end
