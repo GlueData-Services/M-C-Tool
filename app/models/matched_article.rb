db31516048bb625dad642b5eedb09f186f7fe5f7
Marc Howell <sapient@microbump.com>
2022-10-13 16:35:59 +0200

    Add new stats areas
    Adjust styling for home index
    Change navigation

--- app/models/matched_article.rb
+++ app/models/matched_article.rb
@@ -1,5 +1,5 @@
 class MatchedArticle < ApplicationRecord
   audited
-  belongs_to :match
+  belongs_to :match, counter_cache: true
   belongs_to :mara, foreign_key: :prefixed_matnr, primary_key: :prefixed_matnr
 end
