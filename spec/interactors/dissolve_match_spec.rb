0c376d4927e227178315e8ef9603d187c3046a2a
Marc Howell <marc.howell@arktree.io>
2022-10-15 00:52:16 +0200

    Add rspec

--- spec/interactors/dissolve_match_spec.rb
+++ spec/interactors/dissolve_match_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe UpdateMatch, type: :interactor do
+RSpec.describe DissolveMatch, type: :interactor do
   describe '.call' do
     pending "add some examples to (or delete) #{__FILE__}"
   end
