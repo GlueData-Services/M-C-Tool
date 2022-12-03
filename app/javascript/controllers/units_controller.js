081f3b220b47ae06a7f3ac6670b569f8cfd66e04
Marc Howell <marc.howell@arktree.io>
2022-12-03 23:19:41 +0200

    Variants

--- app/javascript/controllers/units_controller.js
+++ app/javascript/controllers/units_controller.js
@@ -1,51 +1,64 @@
-import {Controller} from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["ean", "unit", "matnr", "eanField", "uomField", "overrideUnit"]
+  static targets = [
+    "ean",
+    "unit",
+    "matnr",
+    "eanField",
+    "uomField",
+    "overrideUnit",
+  ];
 
-    connect() {
-        this.checkUnits()
+  connect() {
+    this.checkUnits();
+  }
+
+  checkUnits() {
+    const valid = ["EA", "PAK", "CAR", "CS", "LAY", "PAL"];
+    if (!valid.includes(this.unitTarget.value)) {
+      this.unitTarget.classList.add("text-danger");
+    } else {
+      this.unitTarget.classList.remove("text-danger");
     }
+  }
 
-    checkUnits() {
-        const valid = ["EA", "PAK", "CAR", "CS", "LAY", "PAL"]
-        if (!valid.includes(this.unitTarget.value)) {
-            this.unitTarget.classList.add('text-danger')
-        } else {
-            this.unitTarget.classList.remove('text-danger')
-        }
+  selectEan(event) {
+    this.eanTarget.value = event.params.value;
+    this.eanFieldTargets.forEach(function (el) {
+      el.classList.remove("text-success");
+    });
+    event.target.classList.add("text-success");
+  }
+
+  selectUnit(event) {
+    this.unitTarget.value = event.params.value;
+    this.uomFieldTargets.forEach(function (el) {
+      el.classList.remove("text-success", "fw-bold");
+    });
+    this.checkUnits();
+
+    event.target.classList.add("text-success", "fw-bold");
+  }
+
+  selectRecord(event) {
+    this.matnrTarget.value = event.params.value;
+    this.unitTarget.value = event.params.unit;
+    if (event.params.ean) {
+      this.eanTarget.value = event.params.ean;
     }
+    this.checkUnits();
+  }
 
-    selectEan(event) {
-        this.eanTarget.value = event.params.value;
-        this.eanFieldTargets.forEach(function(el){el.classList.remove("text-success")})
-        event.target.classList.add("text-success")
+  toggleUnitsOverride(event) {
+    if (event.target.checked) {
+      this.unitTarget.disabled = true;
+      this.overrideUnitTarget.disabled = false;
+      this.overrideUnitTarget.classList.remove("d-none");
+    } else {
+      this.unitTarget.disabled = false;
+      this.overrideUnitTarget.disabled = true;
+      this.overrideUnitTarget.classList.add("d-none");
     }
-
-    selectUnit(event) {
-        this.unitTarget.value = event.params.value;
-        this.uomFieldTargets.forEach(function(el){ el.classList.remove("text-success", "fw-bold") })
-        this.checkUnits()
-
-        event.target.classList.add("text-success", "fw-bold")
-    }
-
-    selectRecord(event) {
-        this.matnrTarget.value = event.params.value
-        this.unitTarget.value = event.params.unit
-        this.eanTarget.value = event.params.ean
-        this.checkUnits()
-    }
-
-    toggleUnitsOverride(event) {
-        if (event.target.checked) {
-            this.unitTarget.disabled = true
-            this.overrideUnitTarget.disabled = false
-            this.overrideUnitTarget.classList.remove('d-none')
-        } else {
-            this.unitTarget.disabled = false
-            this.overrideUnitTarget.disabled = true
-            this.overrideUnitTarget.classList.add('d-none')
-        }
-    }
+  }
 }
--- app/javascript/interactions.js
+++ app/javascript/interactions.js
@@ -1,42 +1,98 @@
-$(document).on('turbo:load', function(){
-   // $('table').bootstrapTable();
-   // $("table").resizableColumns();
+$(document).on("turbo:load", function () {
+  // $('table').bootstrapTable();
+  // $("table").resizableColumns();
 
-   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-   })
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 
-   $(".barcode").hover(function(e){
-      let code_class = ".barcode-" + $(this).data('barcode');
-      $(code_class).addClass('active-code');
-   }, function(e){
-      let code_class = ".barcode-" + $(this).data('barcode');
-      $(code_class).removeClass('active-code');
-   });
+  $(".barcode").hover(
+    function (e) {
+      let code_class = ".barcode-" + $(this).data("barcode");
+      $(code_class).addClass("active-code");
+    },
+    function (e) {
+      let code_class = ".barcode-" + $(this).data("barcode");
+      $(code_class).removeClass("active-code");
+    }
+  );
 
-   const tabs = document.querySelectorAll('[data-bs-toggle="tab"]')
-   tabs.forEach(el => {
-      el.addEventListener('shown.bs.tab', event => {
-         sessionStorage.setItem('openTab', el.dataset.bsTarget)
-      })
-   })
+  $(".variant").hover(
+    function (e) {
+      let currVal = $(this).data("value");
+      $("." + currVal).addClass("active-code");
+    },
+    function (e) {
+      let currVal = $(this).data("val");
+      $(".variant").removeClass("active-code");
+    }
+  );
 
-   let el = $(`[data-bs-target="${sessionStorage.getItem('openTab')}"]`)[0]
-   if (el) {
-      bootstrap.Tab.getOrCreateInstance(el).show()
-   } else if (tabs[0]) {
-      bootstrap.Tab.getOrCreateInstance(tabs[0]).show()
-   }
+  /*
+                  Handle quick buttons for setting variation position
+                   */
+  $(".var_plus").click(function (e) {
+    e.preventDefault();
 
-   const triggerTabList = document.querySelectorAll('#myTab button')
-   triggerTabList.forEach(triggerEl => {
-      const tabTrigger = new bootstrap.Tab(triggerEl)
+    let max = getMaxPosition();
+    console.log("Max", max);
+    if (max === 0) {
+      max = 1;
+    }
 
-      triggerEl.addEventListener('click', event => {
-         event.preventDefault()
-         tabTrigger.show()
-      })
-   })
-})
+    $(this).parent().find(".position").val(max);
+  });
 
+  $(".var_plus_one").click(function (e) {
+    e.preventDefault();
+
+    let max = getMaxPosition();
+    if (max === 0) {
+      max = 1;
+    } else {
+      max += 1;
+    }
+    $(this).parent().find(".position").val(max);
+  });
+
+  function getMaxPosition() {
+    let max = 0;
+    $(".position").each(function (e, el) {
+      if (Number(el.value) > max) {
+        max = Number(el.value);
+      }
+    });
+    return max;
+  }
+
+  /*
+                  END Handle quick buttons for setting variation position
+                   */
+
+  const tabs = document.querySelectorAll('[data-bs-toggle="tab"]');
+  tabs.forEach((el) => {
+    el.addEventListener("shown.bs.tab", (event) => {
+      sessionStorage.setItem("openTab", el.dataset.bsTarget);
+    });
+  });
+
+  let el = $(`[data-bs-target="${sessionStorage.getItem("openTab")}"]`)[0];
+  if (el) {
+    bootstrap.Tab.getOrCreateInstance(el).show();
+  } else if (tabs[0]) {
+    bootstrap.Tab.getOrCreateInstance(tabs[0]).show();
+  }
+
+  const triggerTabList = document.querySelectorAll("#myTab button");
+  triggerTabList.forEach((triggerEl) => {
+    const tabTrigger = new bootstrap.Tab(triggerEl);
+
+    triggerEl.addEventListener("click", (event) => {
+      event.preventDefault();
+      tabTrigger.show();
+    });
+  });
+});
