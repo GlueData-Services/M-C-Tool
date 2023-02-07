75a536b86b1a6ca110fc51a4ff0557915f0498d6
Mark Howell <mark@agrigateone.com>
2023-02-08 00:46:58 +0200

    Re-add db auth
    Signed-off-by: Mark Howell <mark@agrigateone.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :timeoutable and :omniauthable
   # devise :ldap_authenticatable, :recoverable, :rememberable, :lockable, :trackable
-  devise :recoverable, :rememberable, :lockable, :trackable
+  devise :database_authenticatable, :recoverable, :rememberable, :lockable, :trackable
 
   def is_admin?
     self.user_type.inquiry.admin?
