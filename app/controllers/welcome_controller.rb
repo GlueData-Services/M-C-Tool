dacfec7cc1d11fe0ad386e654c74e10213e0d204
Mark Howell <mark@agrigateone.com>
2023-02-08 00:38:56 +0200

    Change to skip before action
    Signed-off-by: Mark Howell <mark@agrigateone.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-
+  skip_before_action :authenticate_user!
+  
   def index
     if params[:q].present?
       @search = Article.search(params[:q])
